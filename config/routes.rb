982a391d26ecc7cab1a50c91a8868919e6bcc116
Chris Hewitt <agop5134@gmail.com>
2022-02-26 11:55:57 -0700

    heroku landing page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root "/"
+  root "/", to: 'welcome_controller#welcome'
   namespace :api do
     namespace :v1 do
       resources :users, only: [:show]
