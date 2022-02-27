02c12dd27a10a9d55f4681fda9cd409b37a2f680
Chris Hewitt <agop5134@gmail.com>
2022-02-26 18:36:04 -0700

    Add lat/long to controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,5 +1,7 @@
-class Api::V1::UsersController < ApplicationController 
-  def show 
-    render(json: UserSerializer.new(User.find(params[:id])))
+class Api::V1::UsersController < ApplicationController
+  def show
+  user = User.find(params[:id])
+  user.add_coordinates
+  render(json: UserSerializer.new(user))
   end
 end
--- spec/requests/api/v1/users_request_spec.rb
+++ spec/requests/api/v1/users_request_spec.rb
@@ -1,15 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe 'user requests' do
-  it 'renders user to json', :vcr do 
+  it 'renders user to json', :vcr do
     user = FactoryBot.create(:user, street_address: "19072 Ave 300", city: "Exeter", state: "California", zipcode: "93221")
 
     get api_v1_user_path(user)
-    
+
     parsed = JSON.parse(response.body , symbolize_names: true)[:data]
-    
+
     expect(response).to be_successful
-    
+
     expect(parsed[:id]).to be_a(String)
     expect(parsed[:attributes][:user_name]).to be_a(String)
     expect(parsed[:attributes][:email]).to be_a(String)
@@ -18,5 +18,7 @@ RSpec.describe 'user requests' do
     expect(parsed[:attributes][:state]).to be_a(String)
     expect(parsed[:attributes][:zipcode]).to be_a(String)
     expect(parsed[:attributes][:activity_preferences]).to be_a(String)
+    expect(parsed[:attributes][:latitude]).to be_a(Float)
+    expect(parsed[:attributes][:longitude]).to be_a(Float)
   end
 end
