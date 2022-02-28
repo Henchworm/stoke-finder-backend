3ef1035bb6ee7d7df9c0421d28ab6e0492314b4b
Kerri Hoffmann <kerri.hoffmann@gmail.com>
2022-02-28 15:28:29 -0700

    Complete spec and service

--- app/services/recreation_service.rb
+++ app/services/recreation_service.rb
@@ -4,13 +4,16 @@ class RecreationService
     headers: {'apiKey' => ENV['rec_api_key']})
   end
 
-  def self.get_rec_area(latitude, longitude , radius = 50) #activity/activities
+  def self.get_rec_area(latitude, longitude, radius = 50.00, activities)
     response = conn.get("/api/v1/recareas?") do |request|
       request.params['limit'] = "10"
-      request.params['state'] = state
+      request.params['latitude'] = latitude
+      request.params['longitude'] = longitude
       request.params['radius'] = radius
-      # request.params['activity'] = activity
+      request.params['full'] = true
+      request.params['activity'] = activities
     end
+
     data = JSON.parse(response.body, symbolize_names: true)
   end
 end
--- spec/services/recreation_service_spec.rb
+++ spec/services/recreation_service_spec.rb
@@ -3,49 +3,46 @@ require 'rails_helper'
 RSpec.describe RecreationService do
   it 'can search for a rec area by multiple parameters' do
      VCR.use_cassette('rec_areas_search') do
-      user = create(:user)
+      activities = "HIKING BIKING"
+      user = create(:user, street_address: "1634 18th St", city: "Denver", state: "CO", zipcode: "80202", activity_preferences: activities)
+
+      user.add_coordinates
 
       latitude = user.latitude
       longitude = user.longitude
       radius = 10
-      #default =
-      # lat = 39.8916537
-      # long =  -105.7630624
-      # activity = "Hiking"
-      # activities = ["Hiking", "Biking", ""]
-      #keyword or activity ID
+      activities = [user.activity_preferences]
 
-      expect(RecreationService.get_rec_area(state, radius)).to be_a(Hash)
-      expect(RecreationService.get_rec_area(state, radius)[:RECDATA]).to be_an(Array)
-      expect(RecreationService.get_rec_area(state, radius)[:RECDATA][0]).to be_a(Hash)
+      expect(RecreationService.get_rec_area(latitude,longitude,radius,activities)).to be_a(Hash)
+      expect(RecreationService.get_rec_area(latitude,longitude,radius,activities)[:RECDATA]).to be_an(Array)
+      expect(RecreationService.get_rec_area(latitude,longitude,radius,activities)[:RECDATA][0]).to be_a(Hash)
 
-      rec_data = RecreationService.get_rec_area(state, radius)[:RECDATA][0]
+      rec_data = RecreationService.get_rec_area(latitude,longitude,radius,activities)[:RECDATA][0]
 
       expect(rec_data).to have_key(:RecAreaID)
       expect(rec_data[:RecAreaID]).to be_a(String)
       expect(rec_data).to have_key(:RecAreaName)
-      expect(rec_data)[:RecAreaName]).to be_a(String)
-      # expect(rec_data).to have_key(:RecAreaDescription)
-      # expect(rec_data)[:RecAreaDescription].to be_a(String)
-      # expect((rec_data).to have_key(:RECAREAADDRESS)
-      # expect(rec_data)[:RECAREAADDRESS][0].to be_a(Hash)
-      #             #need to build out keys for address????
-      # expect(rec_data).to have_key(:RecAreaDirections)
-      # expect(rec_data)[:RecAreaDirections].to be_a(String)
-      # expect(rec_data).to have_key(:RecAreaPhone)
-      # expect(rec_data)[:RecAreaPhone].to be_a(String)
-      # expect(rec_data).to have_key(:RecAreaEmail)
-      # expect(rec_data)[:RecAreaEmail].to be_a(String)
-      # expect(rec_data).to have_key(:Reservable)
-      # expect(rec_data)[:Reservable].to be_a(Boolean)
-      # expect(rec_data).to have_key(ACTIVITY)
-      # expect(rec_data)[ACTIVITY][0].to be_a(Hash)
-          #need to build out activity data
-          #coorinates??? key "GEOJSON" => Hash OR
-          #"RecAreaLongitude" => float
-          #"RecAreaLatitude" => float
-          # key "LINK"
-          # key "MEDIA"
+      expect(rec_data[:RecAreaName]).to be_a(String)
+      expect(rec_data).to have_key(:RecAreaDescription)
+      expect(rec_data[:RecAreaDescription]).to be_a(String)
+      expect(rec_data).to have_key(:RECAREAADDRESS)
+      expect(rec_data[:RECAREAADDRESS][0]).to be_a(Hash)
+      expect(rec_data).to have_key(:RecAreaDirections)
+      expect(rec_data[:RecAreaDirections]).to be_a(String)
+      expect(rec_data).to have_key(:RecAreaPhone)
+      expect(rec_data[:RecAreaPhone]).to be_a(String)
+      expect(rec_data).to have_key(:Reservable)
+      #expect(rec_data[:Reservable]).to be_a(String)
+      expect(rec_data).to have_key(:ACTIVITY)
+      expect(rec_data[:ACTIVITY][0]).to be_a(Hash)
+      expect(rec_data).to have_key(:RecAreaLongitude)
+      expect(rec_data[:RecAreaLongitude]).to be_a(Float)
+      expect(rec_data).to have_key(:RecAreaLatitude)
+      expect(rec_data[:RecAreaLatitude]).to be_a(Float)
+      expect(rec_data).to have_key(:MEDIA)
+      expect(rec_data[:MEDIA]).to be_an(Array)
+      expect(rec_data).to have_key(:LINK)
+      expect(rec_data[:LINK]).to be_a(Array)
     end
   end
 end
