275086db172d617a5c0c794c09a4afe7fd953af0
Robin Dittrich <robinodittrich@gmail.com>
2022-03-01 15:09:22 -0800

    Add robust validationss to user spec.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,18 +1,56 @@
 require 'rails_helper'
 RSpec.describe User, type: :model do
-
   describe 'validations' do
-    it { should validate_presence_of :user_name }
-    it { should validate_uniqueness_of :email }
-    it { should validate_presence_of :password }
-    it { should validate_presence_of :password_digest }
-    it { should have_secure_password }
-    it { should validate_presence_of :access }
-    it { should validate_presence_of :street_address }
-    it { should validate_presence_of :city }
-    it { should validate_presence_of :state }
-    it { should validate_presence_of :zipcode }
-    it { should validate_presence_of :activity_preferences }
+    let(:user)  { build :user }
+    describe '#user_name' do
+      it { should validate_presence_of(:user_name) }
+      it { should_not allow_value(nil).for(:user_name) }
+    end
+
+    describe '#email' do
+      it { should validate_presence_of(:email) }
+      it { should_not allow_value(nil).for(:email) }
+      it { should validate_uniqueness_of(:email) }
+    end
+
+    describe '#password' do
+      it { should validate_presence_of(:password_digest)}
+      it 'tests a new user creation' do
+        expect(user).to_not have_attribute(:password)
+        expect(user.password_digest).to_not eq('password123')
+      end
+      it { should have_secure_password }
+    end
+
+    describe '#access' do
+      it { should validate_presence_of(:access) }
+      it { should_not allow_value(nil).for(:access) }
+    end
+
+    describe '#street_address' do
+      it { should validate_presence_of(:street_address) }
+      it { should_not allow_value(nil).for(:street_address) }
+    end
+
+    describe '#city' do
+      it { should validate_presence_of(:city) }
+      it { should_not allow_value(nil).for(:city) }
+    end
+
+    describe '#state' do
+      it { should validate_presence_of(:state) }
+      it { should_not allow_value(nil).for(:state) }
+    end
+
+    describe '#zipcode' do
+      it { should validate_presence_of(:zipcode) }
+      it { should_not allow_value(nil).for(:zipcode) }
+    end
+
+    describe '#activity_preferences' do
+      it { should validate_presence_of(:activity_preferences) }
+      it { should_not allow_value(nil).for(:activity_preferences) }
+    end
   end
 
   describe "relationships" do
