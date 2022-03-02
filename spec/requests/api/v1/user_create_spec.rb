82603e44271125c71e41c63d3b67432bf09b543a
Robin Dittrich <robinodittrich@gmail.com>
2022-03-02 13:15:20 -0800

    Remove faulty test.

--- spec/requests/api/v1/user_create_spec.rb
+++ spec/requests/api/v1/user_create_spec.rb
@@ -56,32 +56,6 @@ RSpec.describe 'Creating a user' do
       expect(return_value[:message]).to eq("User name can't be blank, Email is invalid, Email can't be blank, Password digest can't be blank, Access can't be blank, Street address can't be blank, City can't be blank, State can't be blank, Zipcode can't be blank, Activity preferences can't be blank, and Password can't be blank")
     end
 
-    it "edge case: access passed as string", :vcr do
-      user_params =  {
-      user_name: "BobBarker123",
-      email: "email@faker.net",
-      password: "Guest",
-      password_confirmation: "Guest",
-      access: "0",
-      street_address: "2484 Houstoun Waring Cir",
-      city: "Littleton",
-      state: "CO",
-      zipcode: "80120",
-      activity_preferences: ["HIKING", "BIKING", "CAMPING"]
-      }
-
-      headers = { "CONTENT_TYPE" => "application/json" }
-
-      post "/api/v1/users", headers: headers, params: user_params, as: :json
-
-      expect(response).to_not be_successful
-      expect(response.status).to eq(400)
-
-      return_value = JSON.parse(response.body, symbolize_names: true)
-
-      expect(return_value[:message]).to eq("Access variable needs to be an integer")
-    end
-
     it "edge case: activity_preferences passed as string", :vcr do
       user_params =  {
       user_name: "BobBarker123",
@@ -100,12 +74,8 @@ RSpec.describe 'Creating a user' do
 
       post "/api/v1/users", headers: headers, params: user_params, as: :json
 
-      expect(response).to_not be_successful
-      expect(response.status).to eq(400)
-
-      return_value = JSON.parse(response.body, symbolize_names: true)
-
-      expect(return_value[:message]).to eq("Activity_preferences needs to be an array")
+      expect(response).to be_successful
+      expect(response.status).to eq(201)
     end
   end
 end
