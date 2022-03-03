d1b07c8c597ea8e06157b1f87c313f950a20daa8
Chris Hewitt <agop5134@gmail.com>
2022-03-02 23:14:41 -0700

    Minor test refactor for delete/show

--- spec/requests/api/v1/adventures/adventure_destroy_spec.rb
+++ spec/requests/api/v1/adventures/adventure_destroy_spec.rb
@@ -3,15 +3,14 @@ require 'rails_helper'
 RSpec.describe "Delete an adventure request" do
   describe 'deletes an adventure' do
     it 'happy path' do
-      user = FactoryBot.create(:user)
-      adventure = Adventure.create!(user_id: user.id,
-                                    rec_area_id: 1,
-                                    activities: "Hiking, Fishing, Boating",
-                                    comment: "cool rocks",
-                                    favorite: true,
-                                    date: DateTime.current
-                                  )
-
+    adventure = Adventure.create!(
+          rec_area_id: 1,
+          guest_email_addresses: "bob@google.com, john@reddit.com",
+          activities: "Hiking, Fishing, Boating",
+          comment: "cool rocks",
+          favorite: true,
+          date: DateTime.current
+        )
       expect(Adventure.count).to eq(1)
 
       delete "/api/v1/adventures/#{adventure.id}"
--- spec/requests/api/v1/adventures/adventure_show_spec.rb
+++ spec/requests/api/v1/adventures/adventure_show_spec.rb
@@ -4,13 +4,25 @@ RSpec.describe "Show an adventure request" do
   describe 'shows an adventure' do
     it 'happy path' do
       user = FactoryBot.create(:user)
-      adventure = Adventure.create!(user_id: user.id,
-                                    rec_area_id: 1,
-                                    activities: "Hiking, Fishing, Boating",
-                                    comment: "cool rocks",
-                                    favorite: true,
-                                    date: DateTime.current
-                                  )
+      adventure = Adventure.create!(
+            rec_area_id: 1,
+            guest_email_addresses: "bob@google.com, john@reddit.com",
+            activities: "Hiking, Fishing, Boating",
+            comment: "cool rocks",
+            favorite: true,
+            date: DateTime.current
+          )
+
+      adventure_params = {
+      user_id: user.id,
+          #user id from session
+      rec_area_id: 1,
+      guest_email_addresses: "bob@google.com, john@reddit.com",
+      activities: "Hiking, Fishing, Boating",
+      comment: "cool rocks",
+      favorite: true,
+      date: DateTime.current
+      }
 
       headers = { "CONTENT_TYPE" => "application/json" }
       post "/api/v1/adventures", headers: headers, params: adventure_params, as: :json
@@ -24,20 +36,17 @@ RSpec.describe "Show an adventure request" do
 
       adventure_data = adventure[:data]
 
-      expect(adventure[:attributes]).to have_key(:rec_area_id)
-      expect(adventure[:attributes][:rec_area_id]).to be_an(Integer)
+      expect(adventure_data[:attributes]).to have_key(:guest_email_addresses)
+      expect(adventure_data[:attributes][:guest_email_addresses]).to be_an(String)
 
-      expect(adventure[:attributes]).to have_key(:guest_email_addresses)
-      expect(adventure[:attributes][:guest_email_addresses]).to be_an(String)
+      expect(adventure_data[:attributes]).to have_key(:activities)
+      expect(adventure_data[:attributes][:activities]).to be_an(String)
 
-      expect(adventure[:attributes]).to have_key(:activities)
-      expect(adventure[:attributes][:activities]).to be_an(String)
+      expect(adventure_data[:attributes]).to have_key(:comment)
+      expect(adventure_data[:attributes][:comment]).to be_an(String)
 
-      expect(adventure[:attributes]).to have_key(:comment)
-      expect(adventure[:attributes][:comment]).to be_an(String)
-
-      expect(adventure[:attributes]).to have_key(:favorite)
-      expect(adventure[:attributes][:favorite]).to be_an(Boolean)
+      expect(adventure_data[:attributes]).to have_key(:favorite)
+      expect(adventure_data[:attributes][:favorite]).to eq(true)
     end
   end
 end
